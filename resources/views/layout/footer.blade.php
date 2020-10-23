f678f33a8d8ebe3d65f636ded2e21e55db002976
Duy Nguyen <duy210697@gmail.com>
2020-10-24 00:01:58 +0700

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -22,3 +22,4 @@
 
     </div>
 </footer>
+<iframe src="https://www6.cbox.ws/box/?boxid=848660&boxtag=zbFiKx" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>
